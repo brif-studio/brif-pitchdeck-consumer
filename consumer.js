017aaaffc2e1afb39399c1e4206d9b8d1033b9c5
Rıfat Kaşıkcı <rifatkasikci@gmail.com>
2023-08-07 18:01:30 +0300

    Added error handling by events for connection loses

--- consumer.js
+++ consumer.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 const publisher = async () => {
     try {
-        const connection = await amqplib.connect({
+        let connection = await amqplib.connect({
             protocol: 'amqp',
             hostname: process.env.RABBITMQ_HOST,
             port: process.env.RABBITMQ_PORT,
@@ -14,7 +14,51 @@ const publisher = async () => {
             vhost: process.env.RABBITMQ_USERNAME,
             heartbeat: 10
         })
+
+        connection.on('close', async ()=>{
+            connection = await amqplib.connect({
+                protocol: 'amqp',
+                hostname: process.env.RABBITMQ_HOST,
+                port: process.env.RABBITMQ_PORT,
+                username: process.env.RABBITMQ_USERNAME,
+                password: process.env.RABBITMQ_PASSWORD,
+                vhost: process.env.RABBITMQ_USERNAME,
+                heartbeat: 10
+            })
+        })
+
+        connection.on('error', (err) => {
+            console.log(err)
+        })
+
+        connection.on('blocked', (reason) => {
+            console.log(reason)
+        })
+
+
         const channel = await connection.createChannel()
+        
+        channel.on('close', async ()=>{
+            connection = await amqplib.connect({
+                protocol: 'amqp',
+                hostname: process.env.RABBITMQ_HOST,
+                port: process.env.RABBITMQ_PORT,
+                username: process.env.RABBITMQ_USERNAME,
+                password: process.env.RABBITMQ_PASSWORD,
+                vhost: process.env.RABBITMQ_USERNAME,
+                heartbeat: 10
+            })
+        })
+
+        channel.on('error', (err) => {
+            console.log(err)
+        })
+
+        channel.on('blocked', (reason) => {
+            console.log(reason)
+        })
+        
+        
         const exchange = await channel.assertExchange(process.env.EXCHANGE_NAME, 'topic', {
             durable: false
         })
